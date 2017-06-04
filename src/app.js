2798163b0049e52b975fd906135ae22c58077184
likethemammal <dolphin@likethemammal.com>
2017-06-04 01:50:24 -0400

    Initial project files

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Example from './Example.js'
+
+ReactDOM.render(
+    <Example />,
+    document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+import webpack from 'webpack'
+
+import path from 'path'
+
+const plugins = [
+
+]
+
+const config = {
+
+    context: path.resolve(__dirname, './src'),
+
+    entry: {
+        app: './app.js',
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path:  path.resolve(__dirname, './dist'),
+        publicPath: '/public',
+    },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, './src'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                }],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { modules: true },
+                    },
+                ],
+            },
+        ]
+
+    },
+    plugins,
+}
+
+module.exports = config
