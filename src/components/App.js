f18284ee948a5ebc9f64c27203b3d02c794a20e0
Chris Dolphin <cdolphin1991@gmail.com>
2018-01-21 01:53:57 -0800

    Upgrade to react 16 and hook up react-primitives

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 
+import {
+    View,
+    Text,
+} from '@likethemammal/react-primitives'
+
 import connect from './App.connect'
 
 import CheckboxWithLabel from './CheckboxWithLabel'
@@ -17,7 +22,11 @@ class App extends Component {
 
         return <div>
             <CheckboxWithLabel labelOn={true}/>
-
+            <View>
+                <Text>
+                    {'Some example text'}
+                </Text>
+            </View>
         </div>
     }
 }
